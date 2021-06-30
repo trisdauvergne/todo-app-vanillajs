25b253a6a691d6885610fb56b049eada348c9388
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-06-30 17:34:55 +0200

    Added delete list function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListBtn = document.querySelector('[data-delete-list-button]');
 
-const lists = [{
-  id: 1,
-  name: 'name',
-}, {
-  id: 2,
-  name: 'todo',
-}];
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
 const createList = (name) => ({ id: Date.now().toString(), name, tasks: [] });
 
@@ -18,6 +16,11 @@ const clearElement = (element) => {
   }
 };
 
+const save = () => {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+};
+
 const render = () => {
   clearElement(listsContainer);
   lists.forEach((list) => {
@@ -25,9 +28,24 @@ const render = () => {
     listElement.dataset.listId = list.id;
     listElement.classList.add('list-name');
     listElement.innerText = list.name;
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list');
+    }
     listsContainer.appendChild(listElement);
   });
 };
+
+const saveAndRender = () => {
+  save();
+  render();
+};
+
+listsContainer.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
 
 newListForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -36,7 +54,13 @@ newListForm.addEventListener('submit', (e) => {
   const list = createList(listName);
   newListInput.value = '';
   lists.push(list);
-  render();
+  saveAndRender();
+});
+
+deleteListBtn.addEventListener('click', (e) => {
+  lists = lists.filter((list) => list.id !== selectedListId);
+  selectedListId = null;
+  saveAndRender();
 });
 
 render();
